472a4c33b43485edb04185bb3ecadcc0031a3bc0
rycardo <rycardo@users.noreply.github.com>
2017-11-01 14:58:16 -0400

    removed autoreleasing lines
    autoreleasing not allowed in ARC (according to Xcode compile)

--- NSData+Base64.m
+++ NSData+Base64.m
@@ -301,11 +301,10 @@ char *NewBase64Encode(
 		NewBase64Encode([self bytes], [self length], true, &outputLength);
 	
 	NSString *result =
-		[[[NSString alloc]
+		[[NSString alloc]
 			initWithBytes:outputBuffer
 			length:outputLength
-			encoding:NSASCIIStringEncoding]
-		autorelease];
+			encoding:NSASCIIStringEncoding];
 	free(outputBuffer);
 	return result;
 }
@@ -318,11 +317,10 @@ char *NewBase64Encode(
     NewBase64Encode([self bytes], [self length], separateLines, &outputLength);
 	
 	NSString *result =
-    [[[NSString alloc]
+    [[NSString alloc]
       initWithBytes:outputBuffer
       length:outputLength
-      encoding:NSASCIIStringEncoding]
-     autorelease];
+      encoding:NSASCIIStringEncoding];
 	free(outputBuffer);
 	return result;
 }
